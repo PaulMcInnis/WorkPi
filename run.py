de4d28a71181427520ba2fb7cc779f77045ed772
Paul McInnis <paul@darwinai.ca>
2020-03-21 16:40:12 -0400

    made the timer functional

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 """Work logging tool based on raspberry pi TFT TODO: JIRA integ."""
 from collections import namedtuple
-from datetime import datetime
+from datetime import timedelta, datetime
 from enum import Enum
-import os, sys
+import os, sys, sched, time
 from typing import List
 
-from gpiozero import Button
+#from gpiozero import Button
 import pygame
 from pygame.locals import KEYDOWN, K_ESCAPE, K_DOWN, K_UP, K_RETURN, K_BACKSPACE, KMOD_NONE
 
@@ -16,7 +16,7 @@ os.environ["SDL_FBDEV"] = "/dev/fb1"
 os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
 os.environ["SDL_MOUSEDRV"] = "TSLIB"
 
-# Inits
+# Inits TODO: move some of these into WorkDisplay
 SCREEN_HEIGHT = 240
 SCREEN_WIDTH = 320
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -24,6 +24,7 @@ NUM_JOBS_DISPLAYED = 5
 ENC_A_PIN = 14  # Note these are not the actual rpi pin #'s but the BCM #s
 ENC_B_PIN = 15
 SW_PIN = 23
+TIMER_REFRESH_SECONDS = 1
 
 # NOTE: OpenSans license (Apache 2.0) here: https://www.fontsquirrel.com/license/open-sans
 ID_FONT = '/home/pi/WorkPi/OpenSans-Semibold.ttf'  # FIXME these paths are gonna be wrong for users.
@@ -39,6 +40,7 @@ LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e
 SELECTOR_WIDTH = SCREEN_WIDTH - 8
 SELECTOR_HEIGHT = int(SCREEN_HEIGHT / NUM_JOBS_DISPLAYED) - 8
 
+# TODO move into WorkDisplay
 BLACK = (0, 0, 0)
 BG_COLOUR = (39, 40, 34)
 DESC_COLOUR = (230, 219, 116)
@@ -46,16 +48,24 @@ ID_COLOUR = (174, 129, 255)
 HIGHLIGHT_COLOUR = (249, 38, 114)
 TIME_COLOUR = (253, 151, 31)
 
-Job = namedtuple('Job', 'id desc elapsed')
+
+class Job(object):
+    def __init__(self, id: str, desc: str, elapsed: int):
+        """FIXME make elapsed a strftime format."""
+        self.id = id
+        self.desc = desc
+        self.elapsed = elapsed
 
 
 class Direction(Enum):
     UP = 1
     DOWN = 2
 
+
 class DisplayMode(Enum):
     SELECTOR = 1
     TIMER = 2
+
 
 class WorkDisplay(object):
     """Parent class with the fonts etc"""
@@ -65,6 +75,22 @@ class WorkDisplay(object):
         self.desc_font = pygame.font.Font(DESC_FONT, 14)
         self.small_time_font = pygame.font.Font(TIME_FONT, 12)
         self.large_time_font = pygame.font.Font(TIME_FONT, 24)
+
+    def get_days_hours_minutes_string(self, td: timedelta, show_seconds: bool = False) -> str:
+        days = td.days
+        hours, remainder = divmod(td.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        seconds += td.microseconds / 1e6
+        ret_str = ""
+        if days:
+            ret_str += "{}d ".format(days)
+        if hours:
+            ret_str += "{}h ".format(hours)
+        if minutes:
+            ret_str += "{}m ".format(minutes)
+        if show_seconds:
+            ret_str += "{:.0f}s".format(seconds)
+        return ret_str
 
 
 class JobDisplay(WorkDisplay):
@@ -93,9 +119,10 @@ class JobDisplay(WorkDisplay):
                 (DESC_COLOUR)),
             (x + 5, y + 20)
         )
-        # FIXME: use time() here
-        small_time = self.small_time_font.render(str(job.elapsed), 1, (TIME_COLOUR))
-        small_time_width = self.small_time_font.size(str(job.elapsed))[0]
+        small_time = self.small_time_font.render(
+            self.get_days_hours_minutes_string(job.elapsed), 1, (TIME_COLOUR))
+        small_time_width = self.small_time_font.size(
+            self.get_days_hours_minutes_string(job.elapsed))[0]
         screen.blit(
             small_time,
             (x + SELECTOR_WIDTH - 5 - small_time_width, y + 5),
@@ -142,11 +169,31 @@ class TimerDisplay(WorkDisplay):
     def __init__(self, job: Job) -> None:
         super().__init__()
         self.job = job
-        self.time = datetime.now()
+        self.start_time = None
+        self.time_elapsed = None  # this goes into self.job when we stop()
 
     def set_job(self, job: Job) -> None:
         """Update the job"""
         self.job = job
+
+    def start(self) -> None:
+        """Start the timer"""
+        self.start_time = datetime.now()
+        self.time_elapsed = timedelta(0)
+        self.running = True
+
+    def stop(self) -> None:
+        """Stop the timer, and update"""
+        self.start_time = None
+        self.job.elapsed += self.time_elapsed
+        self.time_elapsed = None
+
+    def update_elapsed(self) -> None:
+        """Update self.job's elapsed time based on start time"""
+        if self.start_time:
+            self.time_elapsed = datetime.now() - self.start_time
+        else:
+            raise ValueError("Timer not started")
 
     def draw(self) -> None:
         """Draw the entire logging screen"""
@@ -157,13 +204,17 @@ class TimerDisplay(WorkDisplay):
         round_rect(screen, (5, SCREEN_HEIGHT * .75, SELECTOR_WIDTH, 56), BG_COLOUR, 8)
 
         screen.blit(self.id_font.render(self.job.id, 1, (ID_COLOUR)), (10, 5))
+
+        disp_time_string = self.get_days_hours_minutes_string(
+            self.job.elapsed + self.time_elapsed, show_seconds=True)
+        dis_time_width = self.large_time_font.size(disp_time_string)[0]
         screen.blit(
             self.large_time_font.render(
-                self.job.elapsed,
+                disp_time_string,
                 1,
                 (TIME_COLOUR)
             ),
-            (SELECTOR_WIDTH/2 - 30, SCREEN_HEIGHT * .80)
+            (SELECTOR_WIDTH/2 - dis_time_width // 2, SCREEN_HEIGHT * .80)
         )
 
         # FIXME: this should be replaced with something better, will be hard to change (diff screen)
@@ -205,11 +256,11 @@ if __name__ == "__main__":
 
     # Get job data TODO: Query JIRA
     jobs_list = [
-        Job('DE-3334', LOREM_IPSUM,'0h 0m'),
-        Job('PS-6451', LOREM_IPSUM,'3h 15m'),
-        Job('PS-1121', LOREM_IPSUM,'1h 6m'),
-        Job('DE-7613', LOREM_IPSUM,'11h 6m'),
-        Job('DE-4242', LOREM_IPSUM,'2d 11h 6m'),
+        Job('DE-3334', LOREM_IPSUM, timedelta(days=1,minutes=3,seconds=4)),
+        Job('PS-6451', LOREM_IPSUM, timedelta(days=2,minutes=3,seconds=1)),
+        Job('PS-1121', LOREM_IPSUM, timedelta(days=1,minutes=2,seconds=2)),
+        Job('DE-7613', LOREM_IPSUM, timedelta(days=0,minutes=11,seconds=4)),
+        Job('DE-4242', LOREM_IPSUM, timedelta(days=0,minutes=26,seconds=0)),
     ]
     jobs_list.extend(jobs_list)
 
@@ -221,84 +272,82 @@ if __name__ == "__main__":
     selector.draw()
     update_display = True
 
-    # Init I/O
-    enc_a = Button(ENC_A_PIN, pull_up=True)
-    enc_b = Button(ENC_B_PIN, pull_up=True)
-    def enc_a_rising():
-        if enc_b.is_pressed:
-            pygame.event.post(
-                pygame.event.Event(
-                    pygame.locals.KEYDOWN,
-                    key=K_DOWN,
-                    mod=KMOD_NONE
-                )
-            )
-    def enc_b_rising():
-        if enc_a.is_pressed:
-            pygame.event.post(
-                pygame.event.Event(
-                    pygame.locals.KEYDOWN,
-                    key=K_UP,
-                    mod=KMOD_NONE
-                )
-            )
-    enc_a.when_pressed = enc_a_rising
-    enc_b.when_pressed = enc_b_rising
+    # # Init I/O
+    # enc_a = Button(ENC_A_PIN, pull_up=True)
+    # enc_b = Button(ENC_B_PIN, pull_up=True)
+    # def enc_a_rising():
+    #     if enc_b.is_pressed:
+    #         pygame.event.post(
+    #             pygame.event.Event(
+    #                 pygame.locals.KEYDOWN,
+    #                 key=K_DOWN,
+    #                 mod=KMOD_NONE
+    #             )
+    #         )
+    # def enc_b_rising():
+    #     if enc_a.is_pressed:
+    #         pygame.event.post(
+    #             pygame.event.Event(
+    #                 pygame.locals.KEYDOWN,
+    #                 key=K_UP,
+    #                 mod=KMOD_NONE
+    #             )
+    #         )
+    # enc_a.when_pressed = enc_a_rising
+    # enc_b.when_pressed = enc_b_rising
 
-    button = Button(SW_PIN, pull_up=True)
-    def __enter_or_exit_timer_screen():
-        # pressing rotary encoder raises same event as pressing enter
-        pygame.event.post(
-            pygame.event.Event(
-                pygame.locals.KEYDOWN,
-                key=K_RETURN,
-                mod=KMOD_NONE
-            )
-        )
-    button.when_pressed = __enter_or_exit_timer_screen
+    # button = Button(SW_PIN, pull_up=True)
+    # def __enter_or_exit_timer_screen():
+    #     # pressing rotary encoder raises same event as pressing enter
+    #     pygame.event.post(
+    #         pygame.event.Event(
+    #             pygame.locals.KEYDOWN,
+    #             key=K_RETURN,
+    #             mod=KMOD_NONE
+    #         )
+    #     )
+    # button.when_pressed = __enter_or_exit_timer_screen
 
     # Interact + draw loop
     print("running")
+    last_update_time = time.time()
     while True:
 
-        # Get any events
+        # Get any input events
         pyg_events = pygame.event.get()
         if pyg_events:
             update_display = True
 
-        # Manipulate UI via I/O event type
+        # Manipulate UI via input events
         for event in pyg_events:
-
-            # coords for touch screen  # TODO: make this do something
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
-                print("mouse on pos {}".format(pos))
-
-            # handle keys
-            if event.type == KEYDOWN: # TODO: implement encoder
+            if event.type == KEYDOWN:
                 if mode == DisplayMode.SELECTOR:
                     if event.key == K_ESCAPE:
-                        print("kescape: EXIT TO TERMINAL")
                         sys.exit()
                     elif event.key == K_DOWN:
-                        print("kdown")
                         selector.move_selection(Direction.DOWN)
                     elif event.key == K_UP:
                         selector.move_selection(Direction.UP)
-                        print("kup")
                     elif event.key == K_RETURN:
-                        print("kenter")
-                        # enter timer
+                        # start timer
                         timer.set_job(selector.get_job())
                         mode = DisplayMode.TIMER
+                        timer.start()
 
                 elif mode == DisplayMode.TIMER:
                     if event.key == K_ESCAPE or event.key == K_RETURN:
                         mode = DisplayMode.SELECTOR
-                        print("kescape: EXIT TO SELECTOR")
+                    timer.stop()
 
-        # Draw screen if we saw any events
-        if update_display:
+        # Draw screen if we saw any events or it's been 5 seconds (for the timer)
+        cur_time = time.time()
+        update_timer_elapsed = (
+            mode == DisplayMode.TIMER and cur_time - last_update_time >= TIMER_REFRESH_SECONDS)
+        if update_display or update_timer_elapsed:
+
+            if update_timer_elapsed:
+                timer.update_elapsed()
+
             if mode == DisplayMode.SELECTOR:
                 selector.draw()
             elif mode == DisplayMode.TIMER:
@@ -306,4 +355,6 @@ if __name__ == "__main__":
             else:
                 raise ValueError("UNKNOWN MODE")
             pygame.display.update()
+
+            last_update_time = cur_time
             update_display = False
